2fd01dd86030d9f704805c14d7c83f626ee86049
Ramesh M Nair <ramesh.nair@oliveboard.in>
2024-08-01 18:29:44 +0530

    modelstructured for strip data

--- app/src/main/java/com/ramzmania/tubefy/data/dto/youtubestripper/YoutubeStripResponse.kt
+++ app/src/main/java/com/ramzmania/tubefy/data/dto/youtubestripper/YoutubeStripResponse.kt
@@ -1,4 +1,5 @@
 package com.ramzmania.tubefy.data.dto.youtubestripper
+
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
@@ -51,12 +52,98 @@ data class CompactChannelRenderer(
     @Json(name = "channelId") val channelId: String
 )
 
+/*Shorts data model*/
 @JsonClass(generateAdapter = true)
 data class ReelShelfRenderer(
-    @Json(name = "trackingParams") val trackingParams: String
+    @Json(name = "trackingParams") val trackingParams: String,
+    @Json(name = "items") val shortsList: List<ReelShortsLockupBaseViewModel>
 )
 
 @JsonClass(generateAdapter = true)
-data class VideoWithContextRenderer(
-    @Json(name = "videoId") val videoId: String
+data class ReelShortsLockupBaseViewModel(
+    @Json(name = "shortsLockupViewModel") val trackingParams: ReelShortsLockupViewModel
 )
+
+@JsonClass(generateAdapter = true)
+data class ReelShortsLockupViewModel(
+    @Json(name = "accessibilityText") val accessibilityText: String,
+    @Json(name = "thumbnail") val reelsThumpNail: ReelShortsThumpNail,
+    @Json(name = "onTap") val reelsOnTap: ReelShortsOnTap
+
+
+)
+
+@JsonClass(generateAdapter = true)
+data class ReelShortsOnTap(
+    @Json(name = "innertubeCommand") val innerTubeCommand: ReelShortsInnerTubeCommand
+
+)
+
+@JsonClass(generateAdapter = true)
+data class ReelShortsInnerTubeCommand(
+    @Json(name = "reelWatchEndpoint") val reelWatchEndpoint: ReelShortsReelWatchEndpoint
+
+)
+
+@JsonClass(generateAdapter = true)
+data class ReelShortsReelWatchEndpoint(
+    @Json(name = "videoId") val reelWatchEndpoint: String
+
+)
+
+@JsonClass(generateAdapter = true)
+data class ReelShortsThumpNail(
+    @Json(name = "sources") val thumpList: List<ReelShortsThumpSource>
+
+)
+
+@JsonClass(generateAdapter = true)
+data class ReelShortsThumpSource(
+    @Json(name = "url") val reelShortsThumpNailUrl: String,
+
+    )
+
+
+/*-------END OF Shorts data model--------*/
+
+
+/*------------MAinVideo data model------------*/
+
+@JsonClass(generateAdapter = true)
+data class VideoWithContextRenderer(
+    @Json(name = "videoId") val videoId: String,
+    @Json(name = "headline") val headline: VideoHeadlineRunData,
+    @Json(name = "thumbnail") val thumbnail: VideoHeadlineMainData
+
+)
+
+@JsonClass(generateAdapter = true)
+data class VideoHeadlineRunData(
+    @Json(name = "accessibility") val accessibility: VideoHeadlineRunAccessibilityData
+
+)
+
+@JsonClass(generateAdapter = true)
+data class VideoHeadlineRunAccessibilityData(
+    @Json(name = "accessibilityData") val accessibilityData: VideoHeadlineRunAccessibilityInnerData
+
+)
+
+@JsonClass(generateAdapter = true)
+data class VideoHeadlineRunAccessibilityInnerData(
+    @Json(name = "label") val label: VideoHeadlineRunAccessibilityInnerData
+
+)
+
+@JsonClass(generateAdapter = true)
+data class VideoHeadlineMainData(
+    @Json(name = "thumbnails") val thumbnails: List<VideoThumpUrlData>
+
+)
+
+@JsonClass(generateAdapter = true)
+data class VideoThumpUrlData(
+    @Json(name = "url") val reelShortsThumpNailUrl: String,
+
+    )
+/*------------END OF MAinVideo data model------------*/
