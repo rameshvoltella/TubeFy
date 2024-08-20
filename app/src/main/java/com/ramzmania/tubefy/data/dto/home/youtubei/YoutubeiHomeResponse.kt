65de79aca0b2b4e5aac6ccf3adaaff53f0825c24
Ramesh M Nair <ramesh.nair@oliveboard.in>
2024-08-20 15:46:01 +0530

    model updated for home youtubei

--- app/src/main/java/com/ramzmania/tubefy/data/dto/home/youtubei/YoutubeiHomeResponse.kt
+++ app/src/main/java/com/ramzmania/tubefy/data/dto/home/youtubei/YoutubeiHomeResponse.kt
@@ -1,167 +1,199 @@
 package com.ramzmania.tubefy.data.dto.home.youtubei
 
-import com.ramzmania.tubefy.data.dto.home.youtubei.next.AccessibilityData
-import com.ramzmania.tubefy.data.dto.home.youtubei.next.Content
-import com.ramzmania.tubefy.data.dto.home.youtubei.next.Continuation
-import com.ramzmania.tubefy.data.dto.home.youtubei.next.MusicCarouselShelfBasicHeaderRenderer
-import com.ramzmania.tubefy.data.dto.home.youtubei.next.MusicCarouselShelfRenderer
-import com.ramzmania.tubefy.data.dto.home.youtubei.next.MusicThumbnailRenderer
-import com.ramzmania.tubefy.data.dto.home.youtubei.next.NavigationEndpoint
-import com.ramzmania.tubefy.data.dto.home.youtubei.next.NextContinuationData
-import com.ramzmania.tubefy.data.dto.home.youtubei.next.Run
-import com.ramzmania.tubefy.data.dto.home.youtubei.next.Thumbnail
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class YoutubeiHomeApiResponse(
-    @Json(name = "responseContext") val responseContext: ResponseContext,
-    @Json(name = "contents") val contents: Contents
+data class ApiResponse(
+    @Json(name = "responseContext") val responseContext: ResponseContext?,
+    @Json(name = "contents") val contents: Contents?
 )
 
 @JsonClass(generateAdapter = true)
 data class ResponseContext(
-    @Json(name = "visitorData") val visitorData: String
+    @Json(name = "visitorData") val visitorData: String?
 )
 
 @JsonClass(generateAdapter = true)
 data class Contents(
-    @Json(name = "singleColumnBrowseResultsRenderer") val singleColumnBrowseResultsRenderer: SingleColumnBrowseResultsRenderer
+    @Json(name = "singleColumnBrowseResultsRenderer") val singleColumnBrowseResultsRenderer: SingleColumnBrowseResultsRenderer?
 )
 
 @JsonClass(generateAdapter = true)
 data class SingleColumnBrowseResultsRenderer(
-    @Json(name = "tabs") val tabs: List<Tab>
+    @Json(name = "tabs") val tabs: List<TabRendererWrapper>?
 )
 
 @JsonClass(generateAdapter = true)
-data class Tab(
-    @Json(name = "tabRenderer") val tabRenderer: TabRenderer
+data class TabRendererWrapper(
+    @Json(name = "tabRenderer") val tabRenderer: TabRenderer?
 )
 
 @JsonClass(generateAdapter = true)
 data class TabRenderer(
-    @Json(name = "content") val content: Content
+    @Json(name = "content") val content: SectionListRendererWrapper?
 )
 
 @JsonClass(generateAdapter = true)
-data class Content(
-    @Json(name = "sectionListRenderer") val sectionListRenderer: SectionListRenderer
+data class SectionListRendererWrapper(
+    @Json(name = "sectionListRenderer") val sectionListRenderer: SectionListRenderer?
 )
 
 @JsonClass(generateAdapter = true)
 data class SectionListRenderer(
-    @Json(name = "contents") val contents: List<SectionContent>,
-    @Json(name = "continuations") val continuations: List<Continuation>?
+    @Json(name = "contents") val contents: List<MusicCarouselShelfRendererWrapper>?,
+    @Json(name = "continuations") val continuations: List<ContinuationWrapper>?
 )
 
 @JsonClass(generateAdapter = true)
-data class SectionContent(
-    @Json(name = "musicCarouselShelfRenderer") val musicCarouselShelfRenderer: MusicCarouselShelfRenderer
+data class MusicCarouselShelfRendererWrapper(
+    @Json(name = "musicCarouselShelfRenderer") val musicCarouselShelfRenderer: MusicCarouselShelfRenderer?
 )
 
 @JsonClass(generateAdapter = true)
 data class MusicCarouselShelfRenderer(
-    @Json(name = "header") val header: MusicCarouselShelfBasicHeaderRenderer,
-    @Json(name = "contents") val contents: List<MusicResponsiveListItemRenderer>
+    @Json(name = "header") val header: MusicCarouselShelfBasicHeaderRendererWrapper?,
+    @Json(name = "contents") val contents: List<MusicResponsiveListItemRendererWrapper>?
+)
+
+@JsonClass(generateAdapter = true)
+data class MusicCarouselShelfBasicHeaderRendererWrapper(
+    @Json(name = "musicCarouselShelfBasicHeaderRenderer") val musicCarouselShelfBasicHeaderRenderer: MusicCarouselShelfBasicHeaderRenderer?
 )
 
 @JsonClass(generateAdapter = true)
 data class MusicCarouselShelfBasicHeaderRenderer(
-    @Json(name = "accessibilityData") val accessibilityData: AccessibilityData
+    @Json(name = "accessibilityData") val accessibilityData: AccessibilityDataWrapper?
+)
+
+@JsonClass(generateAdapter = true)
+data class AccessibilityDataWrapper(
+    @Json(name = "accessibilityData") val accessibilityData: AccessibilityData?
 )
 
 @JsonClass(generateAdapter = true)
 data class AccessibilityData(
-    @Json(name = "accessibilityData") val accessibilityData: InnerAccessibilityData
+    @Json(name = "label") val label: String?
 )
 
 @JsonClass(generateAdapter = true)
-data class InnerAccessibilityData(
-    @Json(name = "label") val label: String
+data class MusicResponsiveListItemRendererWrapper(
+    @Json(name = "musicResponsiveListItemRenderer") val musicResponsiveListItemRenderer: MusicResponsiveListItemRenderer?
 )
 
 @JsonClass(generateAdapter = true)
 data class MusicResponsiveListItemRenderer(
-    @Json(name = "thumbnail") val thumbnail: MusicThumbnailRenderer,
-    @Json(name = "flexColumns") val flexColumns: List<MusicResponsiveListItemFlexColumnRenderer>
+    @Json(name = "thumbnail") val thumbnail: MusicThumbnailRendererWrapper?,
+    @Json(name = "flexColumns") val flexColumns: List<MusicResponsiveListItemFlexColumnRendererWrapper>?
+)
+
+@JsonClass(generateAdapter = true)
+data class MusicThumbnailRendererWrapper(
+    @Json(name = "musicThumbnailRenderer") val musicThumbnailRenderer: MusicThumbnailRenderer?
 )
 
 @JsonClass(generateAdapter = true)
 data class MusicThumbnailRenderer(
-    @Json(name = "thumbnail") val thumbnail: Thumbnail
+    @Json(name = "thumbnail") val thumbnail: Thumbnail?,
+    @Json(name = "thumbnailCrop") val thumbnailCrop: String?,
+    @Json(name = "thumbnailScale") val thumbnailScale: String?,
+    @Json(name = "trackingParams") val trackingParams: String?
 )
 
 @JsonClass(generateAdapter = true)
 data class Thumbnail(
-    @Json(name = "thumbnails") val thumbnails: List<ThumbnailDetails>
+    @Json(name = "thumbnails") val thumbnails: List<ThumbnailItem>?
 )
 
 @JsonClass(generateAdapter = true)
-data class ThumbnailDetails(
-    @Json(name = "url") val url: String,
-    @Json(name = "width") val width: Int,
-    @Json(name = "height") val height: Int
+data class ThumbnailItem(
+    @Json(name = "url") val url: String?,
+    @Json(name = "width") val width: Int?,
+    @Json(name = "height") val height: Int?
+)
+
+@JsonClass(generateAdapter = true)
+data class MusicResponsiveListItemFlexColumnRendererWrapper(
+    @Json(name = "musicResponsiveListItemFlexColumnRenderer") val musicResponsiveListItemFlexColumnRenderer: MusicResponsiveListItemFlexColumnRenderer?
 )
 
 @JsonClass(generateAdapter = true)
 data class MusicResponsiveListItemFlexColumnRenderer(
-    @Json(name = "text") val text: Text
+    @Json(name = "text") val text: Text?,
+    @Json(name = "displayPriority") val displayPriority: String?
 )
 
 @JsonClass(generateAdapter = true)
 data class Text(
-    @Json(name = "runs") val runs: List<Run>
+    @Json(name = "runs") val runs: List<Run>?
 )
 
 @JsonClass(generateAdapter = true)
 data class Run(
-    @Json(name = "text") val text: String,
-    @Json(name = "navigationEndpoint") val navigationEndpoint: NavigationEndpoint
+    @Json(name = "text") val text: String?,
+    @Json(name = "navigationEndpoint") val navigationEndpoint: NavigationEndpoint?
 )
 
 @JsonClass(generateAdapter = true)
 data class NavigationEndpoint(
-    @Json(name = "clickTrackingParams") val clickTrackingParams: String,
-    @Json(name = "watchEndpoint") val watchEndpoint: WatchEndpoint
+    @Json(name = "clickTrackingParams") val clickTrackingParams: String?,
+    @Json(name = "watchEndpoint") val watchEndpoint: WatchEndpoint?,
+    @Json(name = "browseEndpoint") val browseEndpoint: BrowseEndpoint?
 )
 
 @JsonClass(generateAdapter = true)
 data class WatchEndpoint(
-    @Json(name = "videoId") val videoId: String,
-    @Json(name = "playlistId") val playlistId: String,
-    @Json(name = "params") val params: String,
-    @Json(name = "loggingContext") val loggingContext: LoggingContext,
-    @Json(name = "watchEndpointMusicSupportedConfigs") val watchEndpointMusicSupportedConfigs: WatchEndpointMusicSupportedConfigs
+    @Json(name = "videoId") val videoId: String?,
+    @Json(name = "playlistId") val playlistId: String?,
+    @Json(name = "params") val params: String?,
+    @Json(name = "loggingContext") val loggingContext: LoggingContext?,
+    @Json(name = "watchEndpointMusicSupportedConfigs") val watchEndpointMusicSupportedConfigs: WatchEndpointMusicSupportedConfigs?
 )
 
 @JsonClass(generateAdapter = true)
 data class LoggingContext(
-    @Json(name = "vssLoggingContext") val vssLoggingContext: VssLoggingContext
+    @Json(name = "vssLoggingContext") val vssLoggingContext: VssLoggingContext?
 )
 
 @JsonClass(generateAdapter = true)
 data class VssLoggingContext(
-    @Json(name = "serializedContextData") val serializedContextData: String
+    @Json(name = "serializedContextData") val serializedContextData: String?
 )
 
 @JsonClass(generateAdapter = true)
 data class WatchEndpointMusicSupportedConfigs(
-    @Json(name = "watchEndpointMusicConfig") val watchEndpointMusicConfig: WatchEndpointMusicConfig
+    @Json(name = "watchEndpointMusicConfig") val watchEndpointMusicConfig: WatchEndpointMusicConfig?
 )
 
 @JsonClass(generateAdapter = true)
 data class WatchEndpointMusicConfig(
-    @Json(name = "musicVideoType") val musicVideoType: String
+    @Json(name = "musicVideoType") val musicVideoType: String?
 )
 
 @JsonClass(generateAdapter = true)
-data class Continuation(
-    @Json(name = "nextContinuationData") val nextContinuationData: NextContinuationData,
-    @Json(name = "clickTrackingParams") val clickTrackingParams: String
+data class BrowseEndpoint(
+    @Json(name = "browseId") val browseId: String?,
+    @Json(name = "params") val params: String?,
+    @Json(name = "browseEndpointContextSupportedConfigs") val browseEndpointContextSupportedConfigs: BrowseEndpointContextSupportedConfigs?
+)
+
+@JsonClass(generateAdapter = true)
+data class BrowseEndpointContextSupportedConfigs(
+    @Json(name = "browseEndpointContextMusicConfig") val browseEndpointContextMusicConfig: BrowseEndpointContextMusicConfig?
+)
+
+@JsonClass(generateAdapter = true)
+data class BrowseEndpointContextMusicConfig(
+    @Json(name = "pageType") val pageType: String?
+)
+
+@JsonClass(generateAdapter = true)
+data class ContinuationWrapper(
+    @Json(name = "nextContinuationData") val nextContinuationData: NextContinuationData?
 )
 
 @JsonClass(generateAdapter = true)
 data class NextContinuationData(
-    @Json(name = "continuation") val continuation: String
+    @Json(name = "continuation") val continuation: String?,
+    @Json(name = "clickTrackingParams") val clickTrackingParams: String?
 )
--- app/src/main/java/com/ramzmania/tubefy/data/dto/home/youtubei/next/YoutubeiHomeNext.kt
+++ app/src/main/java/com/ramzmania/tubefy/data/dto/home/youtubei/next/YoutubeiHomeNext.kt
@@ -1,226 +1,226 @@
 package com.ramzmania.tubefy.data.dto.home.youtubei.next
-
-import com.ramzmania.tubefy.data.dto.home.youtubei.AccessibilityData
-import com.ramzmania.tubefy.data.dto.home.youtubei.Content
-import com.ramzmania.tubefy.data.dto.home.youtubei.Continuation
-import com.ramzmania.tubefy.data.dto.home.youtubei.MusicCarouselShelfBasicHeaderRenderer
-import com.ramzmania.tubefy.data.dto.home.youtubei.MusicCarouselShelfRenderer
-import com.ramzmania.tubefy.data.dto.home.youtubei.MusicThumbnailRenderer
-import com.ramzmania.tubefy.data.dto.home.youtubei.NavigationEndpoint
-import com.ramzmania.tubefy.data.dto.home.youtubei.NextContinuationData
-import com.ramzmania.tubefy.data.dto.home.youtubei.Run
-import com.ramzmania.tubefy.data.dto.home.youtubei.Thumbnail
-import com.squareup.moshi.JsonClass
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
-
-@JsonClass(generateAdapter = true)
-data class YoutubeiHomeContinuationContentRoot(
-    @Json(name = "continuationContents")
-    val continuationContents: ContinuationContents
-)
 @JsonClass(generateAdapter = true)
 data class ContinuationContents(
-    @Json(name = "sectionListContinuation")
-    val sectionListContinuation: SectionListContinuation
+    @Json(name = "continuationContents") val continuationContents: ContinuationContentsData?
+)
+
+@JsonClass(generateAdapter = true)
+data class ContinuationContentsData(
+    @Json(name = "sectionListContinuation") val sectionListContinuation: SectionListContinuation?
 )
 
 @JsonClass(generateAdapter = true)
 data class SectionListContinuation(
-    @Json(name = "contents")
-    val contents: List<Content>,
-    @Json(name = "continuations")
-    val continuations: List<Continuation>
+    @Json(name = "contents") val contents: List<ContentItem?>?,
+    @Json(name = "continuations") val continuations: List<ContinuationData?>?
 )
 
 @JsonClass(generateAdapter = true)
-data class Content(
-    @Json(name = "musicCarouselShelfRenderer")
-    val musicCarouselShelfRenderer: MusicCarouselShelfRenderer
+data class ContentItem(
+    @Json(name = "musicCarouselShelfRenderer") val musicCarouselShelfRenderer: MusicCarouselShelfRenderer?
 )
 
 @JsonClass(generateAdapter = true)
 data class MusicCarouselShelfRenderer(
-    @Json(name = "header")
-    val header: Header,
-    @Json(name = "contents")
-    val contents: List<MusicContent>
+    @Json(name = "header") val header: Header?,
+    @Json(name = "contents") val contents: List<MusicContentItem?>?
 )
 
 @JsonClass(generateAdapter = true)
 data class Header(
-    @Json(name = "musicCarouselShelfBasicHeaderRenderer")
-    val musicCarouselShelfBasicHeaderRenderer: MusicCarouselShelfBasicHeaderRenderer
+    @Json(name = "musicCarouselShelfBasicHeaderRenderer") val musicCarouselShelfBasicHeaderRenderer: MusicCarouselShelfBasicHeaderRenderer?
 )
 
 @JsonClass(generateAdapter = true)
 data class MusicCarouselShelfBasicHeaderRenderer(
-    @Json(name = "accessibilityData")
-    val accessibilityData: AccessibilityData
+    @Json(name = "accessibilityData") val accessibilityData: AccessibilityData?
 )
 
 @JsonClass(generateAdapter = true)
 data class AccessibilityData(
-    @Json(name = "accessibilityData")
-    val accessibilityData: AccessibilityDataLabel
+    @Json(name = "accessibilityData") val accessibilityLabel: LabelData?
 )
 
 @JsonClass(generateAdapter = true)
-data class AccessibilityDataLabel(
-    @Json(name = "label")
-    val label: String
+data class LabelData(
+    @Json(name = "label") val label: String?
 )
 
 @JsonClass(generateAdapter = true)
-data class MusicContent(
-    @Json(name = "musicTwoRowItemRenderer")
-    val musicTwoRowItemRenderer: MusicTwoRowItemRenderer
+data class MusicContentItem(
+    @Json(name = "musicTwoRowItemRenderer") val musicTwoRowItemRenderer: MusicTwoRowItemRenderer?,
+    @Json(name = "musicResponsiveListItemRenderer") val musicResponsiveListItemRenderer: MusicResponsiveListItemRenderer?
 )
 
 @JsonClass(generateAdapter = true)
 data class MusicTwoRowItemRenderer(
-    @Json(name = "thumbnailRenderer")
-    val thumbnailRenderer: ThumbnailRenderer,
-    @Json(name = "title")
-    val title: Title,
-    @Json(name = "thumbnailOverlay")
-    val thumbnailOverlay: ThumbnailOverlay
-)
-
-@JsonClass(generateAdapter = true)
-data class ThumbnailRenderer(
-    @Json(name = "musicThumbnailRenderer")
-    val musicThumbnailRenderer: MusicThumbnailRenderer
+    @Json(name = "thumbnailRenderer") val thumbnailRenderer: MusicThumbnailRenderer?,
+    @Json(name = "title") val title: Title?,
+    @Json(name = "thumbnailOverlay") val thumbnailOverlay: MusicItemThumbnailOverlayRenderer?
 )
 
 @JsonClass(generateAdapter = true)
 data class MusicThumbnailRenderer(
-    @Json(name = "thumbnail")
-    val thumbnail: Thumbnail
+    @Json(name = "musicThumbnailRenderer") val musicThumbnailRenderer: Thumbnail?
 )
 
 @JsonClass(generateAdapter = true)
 data class Thumbnail(
-    @Json(name = "thumbnails")
-    val thumbnails: List<ThumbnailDetail>
+    @Json(name = "thumbnail") val thumbnails: List<ThumbnailData?>?
 )
 
 @JsonClass(generateAdapter = true)
-data class ThumbnailDetail(
-    @Json(name = "url")
-    val url: String,
-    @Json(name = "width")
-    val width: Int,
-    @Json(name = "height")
-    val height: Int
+data class ThumbnailData(
+    @Json(name = "url") val url: String?,
+    @Json(name = "width") val width: Int?,
+    @Json(name = "height") val height: Int?
 )
 
 @JsonClass(generateAdapter = true)
 data class Title(
-    @Json(name = "runs")
-    val runs: List<Run>
+    @Json(name = "runs") val runs: List<RunData?>?
 )
 
 @JsonClass(generateAdapter = true)
-data class Run(
-    @Json(name = "text")
-    val text: String,
-    @Json(name = "navigationEndpoint")
-    val navigationEndpoint: NavigationEndpoint
+data class RunData(
+    @Json(name = "text") val text: String?,
+    @Json(name = "navigationEndpoint") val navigationEndpoint: NavigationEndpoint?
 )
 
 @JsonClass(generateAdapter = true)
 data class NavigationEndpoint(
-    @Json(name = "clickTrackingParams")
-    val clickTrackingParams: String,
-    @Json(name = "browseEndpoint")
-    val browseEndpoint: BrowseEndpoint
+    @Json(name = "clickTrackingParams") val clickTrackingParams: String?,
+    @Json(name = "browseEndpoint") val browseEndpoint: BrowseEndpoint?,
+    @Json(name = "watchEndpoint") val watchEndpoint: WatchEndpoint?
 )
 
 @JsonClass(generateAdapter = true)
 data class BrowseEndpoint(
-    @Json(name = "browseId")
-    val browseId: String,
-    @Json(name = "browseEndpointContextSupportedConfigs")
-    val browseEndpointContextSupportedConfigs: BrowseEndpointContextSupportedConfigs
+    @Json(name = "browseId") val browseId: String?,
+    @Json(name = "browseEndpointContextSupportedConfigs") val browseEndpointContextSupportedConfigs: BrowseEndpointContextSupportedConfigs?
 )
 
 @JsonClass(generateAdapter = true)
 data class BrowseEndpointContextSupportedConfigs(
-    @Json(name = "browseEndpointContextMusicConfig")
-    val browseEndpointContextMusicConfig: BrowseEndpointContextMusicConfig
+    @Json(name = "browseEndpointContextMusicConfig") val browseEndpointContextMusicConfig: BrowseEndpointContextMusicConfig?
 )
 
 @JsonClass(generateAdapter = true)
 data class BrowseEndpointContextMusicConfig(
-    @Json(name = "pageType")
-    val pageType: String
+    @Json(name = "pageType") val pageType: String?
 )
 
 @JsonClass(generateAdapter = true)
-data class ThumbnailOverlay(
-    @Json(name = "musicItemThumbnailOverlayRenderer")
-    val musicItemThumbnailOverlayRenderer: MusicItemThumbnailOverlayRenderer
+data class WatchEndpoint(
+    @Json(name = "videoId") val videoId: String?,
+    @Json(name = "playlistId") val playlistId: String?,
+    @Json(name = "params") val params: String?,
+    @Json(name = "loggingContext") val loggingContext: LoggingContext?,
+    @Json(name = "watchEndpointMusicSupportedConfigs") val watchEndpointMusicSupportedConfigs: WatchEndpointMusicSupportedConfigs?
+)
+
+@JsonClass(generateAdapter = true)
+data class LoggingContext(
+    @Json(name = "vssLoggingContext") val vssLoggingContext: VssLoggingContext?
+)
+
+@JsonClass(generateAdapter = true)
+data class VssLoggingContext(
+    @Json(name = "serializedContextData") val serializedContextData: String?
+)
+
+@JsonClass(generateAdapter = true)
+data class WatchEndpointMusicSupportedConfigs(
+    @Json(name = "watchEndpointMusicConfig") val watchEndpointMusicConfig: WatchEndpointMusicConfig?
+)
+
+@JsonClass(generateAdapter = true)
+data class WatchEndpointMusicConfig(
+    @Json(name = "musicVideoType") val musicVideoType: String?
 )
 
 @JsonClass(generateAdapter = true)
 data class MusicItemThumbnailOverlayRenderer(
-    @Json(name = "background")
-    val background: Background,
-    @Json(name = "content")
-    val content: ContentOverlay
-)
-
-@JsonClass(generateAdapter = true)
-data class Background(
-    @Json(name = "verticalGradient")
-    val verticalGradient: VerticalGradient
+    @Json(name = "background") val background: VerticalGradient?,
+    @Json(name = "content") val content: MusicPlayButtonRenderer?,
+    @Json(name = "contentPosition") val contentPosition: String?,
+    @Json(name = "displayStyle") val displayStyle: String?
 )
 
 @JsonClass(generateAdapter = true)
 data class VerticalGradient(
-    @Json(name = "gradientLayerColors")
-    val gradientLayerColors: List<String>
+    @Json(name = "verticalGradient") val verticalGradient: GradientLayerColors?
 )
 
 @JsonClass(generateAdapter = true)
-data class ContentOverlay(
-    @Json(name = "musicPlayButtonRenderer")
-    val musicPlayButtonRenderer: MusicPlayButtonRenderer
+data class GradientLayerColors(
+    @Json(name = "gradientLayerColors") val gradientLayerColors: List<String?>?
 )
 
 @JsonClass(generateAdapter = true)
 data class MusicPlayButtonRenderer(
-    @Json(name = "playNavigationEndpoint")
-    val playNavigationEndpoint: PlayNavigationEndpoint
+    @Json(name = "playNavigationEndpoint") val playNavigationEndpoint: PlayNavigationEndpoint?,
+    @Json(name = "trackingParams") val trackingParams: String?,
+    @Json(name = "playIcon") val playIcon: Icon?,
+    @Json(name = "pauseIcon") val pauseIcon: Icon?,
+    @Json(name = "iconColor") val iconColor: Int?,
+    @Json(name = "backgroundColor") val backgroundColor: Int?,
+    @Json(name = "activeBackgroundColor") val activeBackgroundColor: Int?,
+    @Json(name = "loadingIndicatorColor") val loadingIndicatorColor: Int?,
+    @Json(name = "playingIcon") val playingIcon: Icon?,
+    @Json(name = "iconLoadingColor") val iconLoadingColor: Int?,
+    @Json(name = "activeScaleFactor") val activeScaleFactor: Double?,
+    @Json(name = "buttonSize") val buttonSize: String?,
+    @Json(name = "rippleTarget") val rippleTarget: String?,
+    @Json(name = "accessibilityPlayData") val accessibilityPlayData: AccessibilityData?,
+    @Json(name = "accessibilityPauseData") val accessibilityPauseData: AccessibilityData?
 )
 
 @JsonClass(generateAdapter = true)
 data class PlayNavigationEndpoint(
-    @Json(name = "clickTrackingParams")
-    val clickTrackingParams: String,
-    @Json(name = "watchPlaylistEndpoint")
-    val watchPlaylistEndpoint: WatchPlaylistEndpoint
+    @Json(name = "clickTrackingParams") val clickTrackingParams: String?,
+    @Json(name = "watchPlaylistEndpoint") val watchPlaylistEndpoint: WatchPlaylistEndpoint?
 )
 
 @JsonClass(generateAdapter = true)
 data class WatchPlaylistEndpoint(
-    @Json(name = "playlistId")
-    val playlistId: String,
-    @Json(name = "params")
-    val params: String
+    @Json(name = "playlistId") val playlistId: String?,
+    @Json(name = "params") val params: String?
 )
 
 @JsonClass(generateAdapter = true)
-data class Continuation(
-    @Json(name = "nextContinuationData")
-    val nextContinuationData: NextContinuationData
+data class Icon(
+    @Json(name = "iconType") val iconType: String?
+)
+
+@JsonClass(generateAdapter = true)
+data class MusicResponsiveListItemRenderer(
+    @Json(name = "thumbnail") val thumbnail: MusicThumbnailRenderer?,
+    @Json(name = "flexColumns") val flexColumns: List<MusicResponsiveListItemFlexColumnRenderer?>?
+)
+
+@JsonClass(generateAdapter = true)
+data class MusicResponsiveListItemFlexColumnRenderer(
+    @Json(name = "musicResponsiveListItemFlexColumnRenderer") val text: Text?,
+    @Json(name = "displayPriority") val displayPriority: String?
+)
+
+@JsonClass(generateAdapter = true)
+data class Text(
+    @Json(name = "text") val text: String?,
+    @Json(name = "runs") val runs: List<RunData?>?
+)
+
+@JsonClass(generateAdapter = true)
+data class ContinuationData(
+    @Json(name = "nextContinuationData") val nextContinuationData: NextContinuationData?
 )
 
 @JsonClass(generateAdapter = true)
 data class NextContinuationData(
-    @Json(name = "continuation")
-    val continuation: String,
-    @Json(name = "clickTrackingParams")
-    val clickTrackingParams: String
+    @Json(name = "continuation") val continuation: String?,
+    @Json(name = "clickTrackingParams") val clickTrackingParams: String?
 )
